174dff224626c32a79802849975cc03d07773e67
Gusb3ll <Kitpipat.0647@gmail.com>
2022-07-19 21:15:24 +0700

    feat: more remove

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -3,7 +3,7 @@ import { buildSchema } from 'type-graphql'
 import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from 'apollo-server-core'
 
-import Logger from '@yuriko/logger'
+import Logger from '@utils/logger'
 
 import vndbResolver from '@resolvers/vndb.resolver'
 import { errorInterceptor, resolveTime } from '@middleware'
@@ -11,13 +11,17 @@ import { errorInterceptor, resolveTime } from '@middleware'
 import app from './app';
 
 (async () => {
+  const middlewares = [
+    errorInterceptor,
+  ]
+
+  if (process.env.NODE_ENV !== 'production')
+    middlewares.push(resolveTime)
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [vndbResolver],
-      globalMiddlewares: [
-        errorInterceptor,
-        resolveTime,
-      ],
+      globalMiddlewares: middlewares,
     }),
     context: ({ req, res }) => ({ req, res }),
     cache: 'bounded',
@@ -34,7 +38,7 @@ import app from './app';
 
   apolloServer.applyMiddleware({ app })
 
-  app.listen(parseInt(process.env.PORT) || 4000, '0.0.0.0', () => {
-    Logger.info(`Server started on port ${process.env.PORT || 4000}`)
+  app.listen(parseInt(process.env.SERVER_PORT) || 8080, process.env.SERVER_HOST || '0.0.0.0', () => {
+    Logger.info(`Server started on  ${process.env.SERVER_HOST || '0.0.0.0'}:${process.env.SERVER_PORT || 8080}`)
   })
 })()
