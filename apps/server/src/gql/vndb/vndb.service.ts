3ff6b1889659ed000b5949c11fc04c756d820e5a
Gusb3ll <Kitpipat.0647@gmail.com>
2022-10-12 17:11:18 +0700

    fix: vndb constructor

--- apps/server/src/gql/vndb/vndb.service.ts
+++ apps/server/src/gql/vndb/vndb.service.ts
@@ -1,17 +1,17 @@
 import VNDB from 'vndb-api'
 import { Injectable } from '@nestjs/common'
 
-const vndb = new VNDB('yuriko', {
-	host: 'api.vndb.org',
-	port: 19535,
-	encoding: 'utf-8',
-	queryLimit: 10,
-	queryInterval: 3000,
-	minConnection: 1,
-	maxConnection: 10,
-	idleTimeoutMillis: 3000,
-	acquireTimeout: 3000
-})
-
 @Injectable()
-export class VndbService {}
+export class VndbService {
+	vndb = new VNDB('yuriko', {
+		host: 'api.vndb.org',
+		port: 19535,
+		encoding: 'utf-8',
+		queryLimit: 10,
+		queryInterval: 3000,
+		minConnection: 1,
+		maxConnection: 10,
+		idleTimeoutMillis: 3000,
+		acquireTimeout: 3000
+	})
+}
